d72c347e3d47df09d32bc2cc5511a53f57154672
kreker36barabashka <tackabiog@hotmail.com>
2025-09-25 09:35:33 -0500

    feat: initial commit for loadBalancer.js

---
+++ loadBalancer.js
@@ -0,0 +1,14 @@
+class LoadBalancer {
+    constructor(servers) {
+        this.servers = servers;
+        this.currentIndex = 0;
+    }
+
+    getNextServer() {
+        const server = this.servers[this.currentIndex];
+        this.currentIndex = (this.currentIndex + 1) % this.servers.length;
+        return server;
+    }
+}
+
+module.exports = LoadBalancer;
